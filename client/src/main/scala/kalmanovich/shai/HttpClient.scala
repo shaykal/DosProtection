417e4f6578a17ae54b3de6fc91c7d110d90fdb01
shai kalmanovich <shaykal2000@yahoo.com>
2017-12-13 20:14:11 +0200

    Fixed main class, added get http options

--- client/src/main/scala/kalmanovich/shai/HttpClient.scala
+++ client/src/main/scala/kalmanovich/shai/HttpClient.scala
@@ -3,8 +3,13 @@ package kalmanovich.shai
 
 import main.scala.kalmanovich.shai.MainApp
 import main.scala.kalmanovich.shai.utils.PropertiesUtils
+import org.apache.http.HttpResponse
+import org.apache.http.client.config.RequestConfig
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.conn.ssl.DefaultHostnameVerifier
+import org.apache.http.impl.NoConnectionReuseStrategy
 import org.apache.http.impl.client.{CloseableHttpClient, HttpClientBuilder}
+import org.apache.http.impl.conn.{DefaultSchemePortResolver, PoolingHttpClientConnectionManager}
 import org.slf4j.{Logger, LoggerFactory}
 
 
@@ -17,20 +22,51 @@ class HttpClient(url : String, id : Int) extends Runnable {
 
 
   def sendRequest(): Unit = {
-    val urlWithId = s"$url/?clientId=$id"
-    getRestContent(urlWithId, 100, 100)
+    val urlWithId = s"$url?clientId=$id"
+    getRestContent(urlWithId, 10, 10)
   }
 
   def getRestContent(url: String,
                      connectionTimeout: Int,
-                     socketTimeout: Int): String = {
+                     socketTimeout: Int): Unit = {
 
-    val client : CloseableHttpClient = HttpClientBuilder.create().build()
-    val request: HttpGet = new HttpGet(url)
+   // val client : CloseableHttpClient = HttpClientBuilder.create().build()
+    //val request: HttpGet = new HttpGet(url)
     log.info(s"url is: $url")
 
-    //val response : HttpResponse = client.execute(request)
-/*
+ //   val response : HttpResponse = client.execute(request)
+
+    val poolingHttpClientConnectionManager : PoolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+    poolingHttpClientConnectionManager.setDefaultMaxPerRoute(100);
+    poolingHttpClientConnectionManager.setMaxTotal(200);
+
+    val cb : HttpClientBuilder = HttpClientBuilder.create()
+      .disableAutomaticRetries()
+      .setConnectionManager(poolingHttpClientConnectionManager)
+      .setSSLHostnameVerifier(new DefaultHostnameVerifier())
+      .setConnectionReuseStrategy(new NoConnectionReuseStrategy())
+      .setSchemePortResolver(DefaultSchemePortResolver.INSTANCE)
+      .setDefaultRequestConfig(
+        RequestConfig.custom()
+          .setConnectTimeout(connectionTimeout)
+          .setSocketTimeout(socketTimeout).build());
+
+    def get(url: String,
+            connectTimeout: Int = 5000,
+            readTimeout: Int = 5000,
+            requestMethod: String = "GET") =
+    {
+      import java.net.{URL, HttpURLConnection}
+      val connection = (new URL(url)).openConnection.asInstanceOf[HttpURLConnection]
+      connection.setConnectTimeout(connectTimeout)
+      connection.setReadTimeout(readTimeout)
+      connection.setRequestMethod(requestMethod)
+      val inputStream = connection.getInputStream
+      val content = io.Source.fromInputStream(inputStream).mkString
+      if (inputStream != null) inputStream.close
+      content
+    }
+    /*
     val httpClient = buildHttpClient(connectionTimeout, socketTimeout)
     val httpResponse = httpClient.execute(new HttpGet(url))
     val entity = httpResponse.getEntity
@@ -41,8 +77,8 @@ class HttpClient(url : String, id : Int) extends Runnable {
       inputStream.close
     }
     httpClient.getConnectionManager.shutdown
-    content*/
-    ???
+    content
+*/
   }
 
   override def run(): Unit = {
--- client/src/main/scala/kalmanovich/shai/MainApp.scala
+++ client/src/main/scala/kalmanovich/shai/MainApp.scala
@@ -1,19 +1,25 @@
 package main.scala.kalmanovich.shai
 
+import java.io.DataInputStream
+import java.util.Scanner
+
 import kalmanovich.shai.HttpClient
 import main.scala.kalmanovich.shai.utils.Consts
+import org.slf4j.{Logger, LoggerFactory}
 
 /**
   * Created by Shai Kalmanovich on 12/13/2017.
   */
 object MainApp  {
 
+  val log: Logger = LoggerFactory.getLogger(this.getClass)
+
   var isStop = false
 
   def main(args: Array[String]): Unit = {
-    println("test")
+    log.info("test")
 
-    val numOfClients = 1 // args(0).toInt
+    val numOfClients = args(0).toInt
 
     for(i <- 1 to numOfClients) {
       val client : HttpClient = HttpClient(Consts.BASE_URL, i)
@@ -22,8 +28,11 @@ object MainApp  {
     }
 
 
-
-
+    log.info("before key stroke")
+    scala.io.StdIn.readLine() // TODO fix for any key
+    //val scanner : Scanner = new java.util.Scanner(System.in)
+    isStop = true
+    log.info("finish")
   }
 
 }
